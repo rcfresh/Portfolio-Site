5d8b84068744c8628f36ba4079b48e837b8eb831
rcfresh <50213097+rcfresh@users.noreply.github.com>
2019-05-04 17:51:59 -0500

    Adding javascript to make hamburger menu show nav links

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi!  My name is Russell.  Welcome to my site!")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
